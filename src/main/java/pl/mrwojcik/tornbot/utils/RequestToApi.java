20b6f4c430322fb465b84dbba2b247deedcab453
Marcin <maethi@gmail.com>
2021-08-09 18:56:36 +0200

    Update environment variables

--- src/main/java/pl/mrwojcik/tornbot/utils/RequestToApi.java
+++ src/main/java/pl/mrwojcik/tornbot/utils/RequestToApi.java
@@ -1,6 +1,7 @@
 package pl.mrwojcik.tornbot.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import pl.mrwojcik.tornbot.entity.Event;
 import pl.mrwojcik.tornbot.entity.Faction;
@@ -14,15 +15,17 @@ public class RequestToApi {
 
     private final ReadJsonToString readJsonToString;
     private final ObjectMapper objectMapper;
+    private final String apiUrl;
 
-    public RequestToApi(ReadJsonToString readJsonToString, ObjectMapper objectMapper) {
+    public RequestToApi(ReadJsonToString readJsonToString, ObjectMapper objectMapper, @Value("${apiUrl}") String apiUrl) {
         this.readJsonToString = readJsonToString;
         this.objectMapper = objectMapper;
+        this.apiUrl = apiUrl;
     }
 
     public List<Event> getEventsList() {
         try {
-            String eventList = readJsonToString.readJsonFromUrl("http://localhost:8081/api/events");
+            String eventList = readJsonToString.readJsonFromUrl(apiUrl + "/api/events");
             List<Event> events = Arrays.asList(objectMapper.readValue(eventList, Event[].class));
             return events;
         } catch (Exception e){
@@ -33,7 +36,7 @@ public class RequestToApi {
 
     public Event getClosestEvent(){
         try {
-            String event = readJsonToString.readJsonFromUrl("http://localhost:8081/api/event/closest");
+            String event = readJsonToString.readJsonFromUrl(apiUrl + "api/event/closest");
             Event event1 = objectMapper.readValue(event, Event.class);
             return event1;
         } catch (Exception e){
@@ -44,7 +47,7 @@ public class RequestToApi {
 
     public List<Member> getYeetList(){
         try {
-            String yeetMemberListJson = readJsonToString.readJsonFromUrl("http://localhost:8081/api/yeet");
+            String yeetMemberListJson = readJsonToString.readJsonFromUrl(apiUrl + "/api/yeet");
             List<Member> yeetList = Arrays.asList(objectMapper.readValue(yeetMemberListJson, Member[].class));
             return yeetList;
         } catch (Exception e){
@@ -55,7 +58,7 @@ public class RequestToApi {
 
     public Faction getFactionInfo(){
         try {
-            String factionJson = readJsonToString.readJsonFromUrl("http://localhost:8081/api/faction");
+            String factionJson = readJsonToString.readJsonFromUrl(apiUrl + "/api/faction");
             Faction faction = objectMapper.readValue(factionJson, Faction.class);
             return faction;
         } catch (Exception e){
